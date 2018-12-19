4be79d51ffd1dadf2d8d661e6dce4cbbced786d1
Web Xianyu <33566940+yj2512@users.noreply.github.com>
2018-12-19 09:20:37 +0800

    Create deepCopy.js

---
+++ deepCopy.js
@@ -0,0 +1,16 @@
+var deepCopy = function(obj) {
+  // 只拷贝对象
+  if (typeof obj !== 'object') return;
+  // 根据obj的类型判断是新建一个数组还是一个对象
+  var newObj = obj instanceof Array ? [] : {};
+  for (var key in obj) {
+    // 遍历obj,并且判断是obj的属性才拷贝
+    if (obj.hasOwnProperty(key)) {
+      // 判断属性值的类型，如果是对象递归调用深拷贝
+      newObj[key] = typeof obj[key] === 'object' ? deepCopy(obj[key]) : obj[key];
+    }
+  }
+  return newObj;
+}
+--------------------- 
+原文：https://blog.csdn.net/qq_37268201/article/details/80448848 
